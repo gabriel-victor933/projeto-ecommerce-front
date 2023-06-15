67d95bae95e750af70699f451c6acb05c61128f4
gabriel-victor933 <gabrielvictor933@usp.br>
2023-06-15 16:09:11 -0300

    feat: adding pages files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import {Suspense, lazy} from "react"
 
 function App() {
 
   return (
-    <div>
-      <h1>Gabriel</h1>
-      <h2>Gabriel</h2>
-    </div>
+    <BrowserRouter>
+    <div>header</div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<div>initial</div>} />
+        <Route path="/hair" element={<div>hair</div>} />
+        <Route path="/body" element={<div>body</div>} />
+        <Route path="/face" element={<div>face</div>} />
+        <Route path="/kits" element={<div>kits</div>} />
+        <Route path="/about" element={<div>about</div>} />
+        <Route path="/contact" element={<div>contact</div>} />
+        <Route path="/checkout" element={<div>checkout</div>} />
+      </Routes>
+      </Suspense>
+      <div>Footer</div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Face.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Face() {
+  return (
+    <div>Face</div>
+  )
+}
